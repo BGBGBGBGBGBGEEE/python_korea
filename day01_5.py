71d621856c1305cb468edb9aa9cfa48ee8c1d45f
BGBGBGBGBGBGEEE <bguchan@gmail.com>
2023-01-08 09:56:20 +0900

    hello

---
+++ day01_5.py
@@ -0,0 +1,20 @@
+변수1 = 10
+변수2 = 5
+print(변수1 + 변수2)
+print(변수1 * 변수2)
+print(int(변수1 / 변수2))
+print(변수1 % 변수2)
+
+print()
+print('=========')
+# 사과의 가격을 구하기
+# price, count
+price = input('사과의 가격: ')
+print('사과의 가격은',price,'원 입니다.')
+count = input('사과의 갯수 ')
+print('총가격은 '  ,int(price)*int(count), ' 입니다.')
+
+# 사과의 갯수는 00개 입니다.
+
+# 총 가격은 0000원 입니다.
+
